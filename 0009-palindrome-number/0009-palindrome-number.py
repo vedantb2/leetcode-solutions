05906502b152d779a16198109810729c3926f050
Vedant <48868398+vedantb2@users.noreply.github.com>
2024-05-10 23:16:49 +0100

    Time: 66 ms (12.73%), Space: 16.6 MB (64.54%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -5,8 +5,3 @@ class Solution:
         for i in range(len(string)-1, -1, -1):
             rString += string[i]
         return string == rString
-        # if x < 0: return False
-        # reversed_a = a.copy()
-        # reversed_a.reverse()
-        # return a == reversed_a
-    
